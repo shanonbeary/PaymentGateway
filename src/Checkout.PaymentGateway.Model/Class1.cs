fed76c2954722ff071c05ad6403d7d000179ecc3
shanon <shanon.beary@gmail.com>
2023-06-10 12:25:10 +1200

    Inital scaffold

---
+++ src/Checkout.PaymentGateway.Model/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Checkout.PaymentGateway.Model;
+public class Class1
+{
+
+}
---
+++ test/Checkout.PaymentGateway.Service.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Checkout.PaymentGateway.Service.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
